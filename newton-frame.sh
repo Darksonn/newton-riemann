d206a58a0c4fe7fbf274e557a1644ab8a920f8d9
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-07-19 13:10:12 +0200

    vastly improved newton-frame.sh

--- newton-frame.sh
+++ newton-frame.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-echo "$1" > func.str
-make ./newton > /dev/null 2>&1
-./newton | sort -n -k2 -k1 | ./rgb-to-ppm/ppm 3840 2160 | convert - "$2" &
+FUNCTION="$1"
+FILEPATTERN="$2"
+FRAMESTART="$3"
+FRAMEEND="$4"
+
+printf '%s' "$FUNCTION" > func.str
+make ./newton
+
+for ((i="$FRAMESTART";i<="$FRAMEEND";i++)); do
+  FILE="$(printf "$FILEPATTERN" $i)"
+  ./newton $i | sort -n -k2 -k1 | ./rgb-to-ppm/ppm 3840 2160 | convert - "$FILE"
+  printf 'completed frame %d/%d (%.02f%%)\n' $i "$FRAMEEND" $(lua -e "print(100*($i - $FRAMESTART + 1) / ($FRAMEEND - $FRAMESTART + 1))")
+done
+
 
