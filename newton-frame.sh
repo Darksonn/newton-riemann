e0ed5dadbea5aa9d29d912fbd4511437a30e5d42
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-07-19 13:18:12 +0200

    Added explanation to newton-frame.sh if too few arguments are used

--- newton-frame.sh
+++ newton-frame.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ "$#" -ne 4 ]; then
+  printf 'Usage: %s function file-pattern frame-start frame-end\n\n' "$0"
+  printf '  function:     The function to make a newton fractal of. Use t for frame number.\n'
+  printf '  file-pattern: A printf style pattern for the filenames of the frames.\n'
+  printf '  frame-start:  The index of the first frame, and the first value used for t.\n'
+  printf '  frame-end:    The index of the last frame, and the last value used for t.\n\n'
+  exit 1
+fi
 
 FUNCTION="$1"
 FILEPATTERN="$2"
