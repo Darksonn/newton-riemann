63da8fe7be4bcfafda0e9e0c80c48c7bae4eef37
Kristoffer Ryhl-Johansen <kristoffer@ryhl.dk>
2016-07-20 15:46:59 +0200

    Added timing to script

--- newton-frame.sh
+++ newton-frame.sh
@@ -16,10 +16,17 @@ FRAMEEND="$4"
 printf '%s' "$FUNCTION" > func.str
 make ./newton
 
+STARTTIME=$(date +%s.%N)
+
 for ((i="$FRAMESTART";i<="$FRAMEEND";i++)); do
   FILE="$(printf "$FILEPATTERN" $i)"
   ./newton $i | sort -n -k2 -k1 | ./rgb-to-ppm/ppm 3840 2160 | convert - "$FILE"
-  printf 'completed frame %d/%d (%.02f%%)\n' $i "$FRAMEEND" $(lua -e "print(100*($i - $FRAMESTART + 1) / ($FRAMEEND - $FRAMESTART + 1))")
+  ENDTIME=$(date +%s.%N)
+  PERCENT=$(echo "100*($i - $FRAMESTART + 1) / ($FRAMEEND - $FRAMESTART + 1)" | bc -l)
+  DIFF=$(echo "($ENDTIME - $STARTTIME)/60" | bc -l)
+  TOTAL=$(echo "($DIFF / ($i-$FRAMESTART+1)) * ($FRAMEEND-$FRAMESTART+1)" | bc -l)
+  REMAIN=$(echo "$TOTAL - $DIFF" | bc -l)
+  printf 'completed frame %d/%d (%.02f%%) (used: %.01fm, total: %.01fm, remaining: %.01fm)\n' $i "$FRAMEEND" "$PERCENT" "$DIFF" "$TOTAL" "$REMAIN"
 done
 
 
