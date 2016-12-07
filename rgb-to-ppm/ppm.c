abcab0da3e10a564b427d695ef3fe1885cebb8ef
snorr <snorredc@gmail.com>
2016-12-07 17:38:58 +0100

    Make stderr output dependent on argv[0]

--- rgb-to-ppm/ppm.c
+++ rgb-to-ppm/ppm.c
@@ -3,8 +3,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static const char *argv0 = "ppm";
 
 static void die(const char *fmt, ...) {
+  fprintf(stderr, "%s: ", argv0);
+
   va_list ap;
 
   va_start(ap, fmt);
@@ -13,18 +18,34 @@ static void die(const char *fmt, ...) {
   exit(EXIT_FAILURE);
 }
 
+static void usage(void) {
+  const char *progname = argv0;
+
+  const char *basename = strrchr(progname, '/');
+  if (basename) {
+    progname = basename + 1;
+  }
+
+  fprintf(stderr, "usage: %s width height\n", progname);
+  exit(EXIT_FAILURE);
+}
+
 int main(int argc, const char **argv) {
+  if (argv[0]) {
+    argv0 = argv[0];
+  }
+
   if (argc != 3) {
-    die("usage: %s width height\n", argv[0]);
+    usage();
   }
 
   uint32_t width, height;
 
   if (sscanf(argv[1], "%" SCNu32, &width) != 1) {
-    die("ppm: invalid width\n");
+    die("invalid width\n");
   }
   if (sscanf(argv[2], "%" SCNu32, &height) != 1) {
-    die("ppm: invalid height\n");
+    die("invalid height\n");
   }
 
   uint32_t num_pixels = width * height;
@@ -36,7 +57,7 @@ int main(int argc, const char **argv) {
     uint8_t r, g, b;
 
     if (scanf(" %" SCNu32 " %" SCNu32 " %" SCNu8 " %" SCNu8 " %" SCNu8, &x, &y, &r, &g, &b) != 5) {
-      die("ppm: could not read pixel %" PRIu32 " of %" PRIu32 "\n", i, num_pixels);
+      die("could not read pixel %" PRIu32 " of %" PRIu32 "\n", i, num_pixels);
     }
 
     printf("%" PRIu8 " %" PRIu8 " %" PRIu8 "\n", r, g, b);
