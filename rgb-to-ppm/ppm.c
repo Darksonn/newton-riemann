04938a97f0c3559821c351777db33b69c773eb46
snorr <snorredc@gmail.com>
2016-12-07 16:48:25 +0100

    Add more error-handling to ppm

--- rgb-to-ppm/ppm.c
+++ rgb-to-ppm/ppm.c
@@ -1,32 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdint.h>
 #include <inttypes.h>
-#include <assert.h>
-#include <math.h>
-#include <string.h>
-#include <complex.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
+static void die(const char *fmt, ...) {
+  va_list ap;
 
-  if (argc != 3) return 1;
-
-  uint32_t a1, a2;
-  uint8_t r,g,b;
-  uint32_t width, height;
-
-  sscanf(argv[1], "%u", &width);
-  sscanf(argv[2], "%u", &height);
-  uint32_t i = 0;
-
-  printf("P3\n%u %u\n256\n", width, height);
-  while (scanf(" %u %u %" SCNu8 " %" SCNu8 " %" SCNu8, &a1, &a2, &r, &g, &b) != EOF) {
-    if (i++ != a1 + a2*width) {
-      fprintf(stderr, "wrong pixel order %u (width: %u, height: %u)\n", i, width, height);
-      return 1;
-    }
-    printf("%" PRIu8 " %" PRIu8 " %" PRIu8 "\n", r, g, b);
-  }
-  return 0;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  va_end(ap);
+  exit(EXIT_FAILURE);
 }
 
+int main(int argc, const char **argv) {
+  if (argc != 3) {
+    die("usage: %s width height\n", argv[0]);
+  }
+
+  uint32_t width, height;
+
+  if (sscanf(argv[1], "%" SCNu32, &width) != 1) {
+    die("ppm: invalid width\n");
+  }
+  if (sscanf(argv[2], "%" SCNu32, &height) != 1) {
+    die("ppm: invalid height\n");
+  }
+
+  uint32_t num_pixels = width * height;
+
+  printf("P3\n%" PRIu32 " %" PRIu32 "\n256\n", width, height);
+
+  for (uint32_t i = 0; i < num_pixels; i++) {
+    uint32_t x, y;
+    uint8_t r, g, b;
+
+    if (scanf(" %" SCNu32 " %" SCNu32 " %" SCNu8 " %" SCNu8 " %" SCNu8, &x, &y, &r, &g, &b) != 5) {
+      die("ppm: could not read pixel %" PRIu32 " of %" PRIu32 "\n", i, num_pixels);
+    }
+
+    printf("%" PRIu8 " %" PRIu8 " %" PRIu8 "\n", r, g, b);
+  }
+
+  return EXIT_SUCCESS;
+}
