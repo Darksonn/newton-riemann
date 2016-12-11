03f3457544ba31291bf530145499efa63b40f1aa
snorr <snorredc@gmail.com>
2016-12-11 17:52:23 +0100

    Make newton-frame.sh POSIX sh-compatible

--- newton-frame.sh
+++ newton-frame.sh
@@ -1,59 +1,113 @@
-#!/bin/bash
+#!/bin/sh
+
+readonly USAGE_FMT="usage: %s function file-pattern frame-start frame-end
+
+  function:     The function to make a Newton fractal of. Use t for frame number.
+  file-pattern: A printf-style pattern for the filenames of the frames.
+  frame-start:  The index of the first frame, and the first value used for t.
+  frame-end:    The index of the last frame, and the last value used for t.
+
+WARNING: This script overwrites func.str!
+"
+
+readonly RESUME_FMT="#!/bin/sh
+
+export PREV_DIFF=\"%s\"
+export PREV_FRAME_START=\"%s\"
+
+'%s' '%s' '%s' '%s' '%s'
+"
+
 if [ "$#" -ne 4 ]; then
-  printf 'Usage: %s function file-pattern frame-start frame-end\n\n' "$0"
-  printf '  function:     The function to make a newton fractal of. Use t for frame number.\n'
-  printf '  file-pattern: A printf style pattern for the filenames of the frames.\n'
-  printf '  frame-start:  The index of the first frame, and the first value used for t.\n'
-  printf '  frame-end:    The index of the last frame, and the last value used for t.\n\n'
+  # shellcheck disable=SC2059
+  printf "${USAGE_FMT}" "$(basename "$0")" >&2
   exit 1
 fi
 
-FUNCTION="$1"
-FILEPATTERN="$2"
-FRAMESTART="$3"
-FRAMEEND="$4"
-
-printf '%s' "$FUNCTION" > func.str
-STARTTIME=$(date +%s.%N)
-printf 'compiling ./newton\n'
-make ./newton > /dev/null
-if [[ $? != 0 ]]; then
-  printf 'Compilation failed.\n'
+die() {
+  (
+    fmt="$1"
+    shift
+    printf "%s: ${fmt}\\n" "$0" "$@" >&2
+  )
   exit 1
-fi
-ENDTIME=$(date +%s.%N)
-printf '%scompiled ./newton in %.01f seconds' "$(tput cuu1)" "$(echo "$ENDTIME - $STARTTIME" | bc -l)"
+}
 
-STARTTIME=$(date +%s.%N)
+readonly CALC_SCALE=5
+calc() {
+  printf 'scale=%d;%s\n' "${CALC_SCALE}" "$*" | bc
+}
 
-if [ -n "$PREVDIFF" ]; then
-  STARTTIME=$(echo "$STARTTIME - $PREVDIFF" | bc -l)
+readonly FUNCTION="$1"
+readonly FILE_PATTERN="$2"
+readonly FRAME_START="$3"
+readonly FRAME_END="$4"
+
+# Compile newton with the specified function.
+
+printf '%s\n' "${FUNCTION}" >func.str
+
+printf 'Compiling newton...\n'
+
+time_start="$(date +%s)"
+make newton >/dev/null || die 'could not compile newton'
+time_end="$(date +%s)"
+
+printf 'Compiled newton in %d seconds.\n' "$(( time_end - time_start ))"
+
+# Check for resumation.
+
+TIME_START="$(date +%s)"
+if [ -n "${PREV_DIFF}" ]; then
+  TIME_START="$(( TIME_START - PREV_DIFF ))"
 fi
-FRAMESTARTREAL="$FRAMESTART"
-if [ -n "$PREVFRAMESTART" ]; then
-  FRAMESTARTREAL="$PREVFRAMESTART"
-  printf '. Resuming from frame %s\n' "$FRAMESTART"
+readonly TIME_START
+
+if [ -z "${FIRST_FRAME}" ]; then
+  export FIRST_FRAME="${FRAME_START}"
 else
-  printf '. Computing first frame.\n'
+  printf 'Resuming from frame %d.\n' "${FRAME_START}"
 fi
 
+# Compute each frame.
 
-for ((i="$FRAMESTART";i<="$FRAMEEND";i++)); do
-  FILE="$(printf "$FILEPATTERN" $i)"
-  FRAMESTARTTIME=$(date +%s.%N)
-  ./newton $i | sort -n -k2 -k1 | ppm/ppm 3840 2160 | convert - "$FILE"
-  ENDTIME=$(date +%s.%N)
-  FRAMEDIFF=$(echo "$ENDTIME-$FRAMESTARTTIME" | bc -l)
-  PERCENT=$(echo "100*($i - $FRAMESTARTREAL + 1) / ($FRAMEEND - $FRAMESTARTREAL + 1)" | bc -l)
-  DIFFSEC=$(echo "$ENDTIME - $STARTTIME" | bc -l)
-  DIFF=$(echo "($DIFFSEC)/60" | bc -l)
-  TOTAL=$(echo "($DIFF / ($i-$FRAMESTARTREAL+1)) * ($FRAMEEND-$FRAMESTARTREAL+1)" | bc -l)
-  REMAIN=$(echo "$TOTAL - $DIFF" | bc -l)
-  Ni=$((i+1))
-  printf '%scompleted frame %d/%d (%.02f%%) (used: %.01fm, total: %.01fm, remaining: %.01fm, this frame: %.01fs) \n' "$(tput cuu1)" $i "$FRAMEEND" "$PERCENT" "$DIFF" "$TOTAL" "$REMAIN" "$FRAMEDIFF"
-  printf '#!/bin/bash\nexport PREVDIFF="%s"\nexport PREVFRAMESTART="%s"\n%s "%s" "%s" "%s" "%s"\nunset PREVDIFF\nunset PREVFRAMESTART\n' "$DIFFSEC" "$FRAMESTARTREAL" "$0" "$FUNCTION" "$FILEPATTERN" "$Ni" "$FRAMEEND" > resume.sh
+i="${FRAME_START}"
+
+while true; do
+  # shellcheck disable=SC2059
+  file="$(printf "${FILE_PATTERN}" "${i}")"
+
+  printf 'Computing frame %d/%d...\n' "${i}" "${FRAME_END}"
+
+  time_start=$(date +%s)
+  ./newton "${i}" | sort -n -k2 -k1 | ppm/ppm 3840 2160 | convert - "${file}" || die 'could not compute frame'
+  time_end="$(date +%s)"
+
+  if [ "${i}" -ge "${FRAME_END}" ]; then
+    break
+  fi
+
+  frame_diff="$(( time_end - time_start ))"
+  percent="$(calc "100 * (${i} - ${FIRST_FRAME} + 1) / (${FRAME_END} - ${FIRST_FRAME} + 1)")"
+
+  diff_sec="$(( time_end - TIME_START ))"
+  diff="$(calc "${diff_sec}/60")"
+
+  total="$(calc "(${diff} / (${i} - ${FIRST_FRAME} + 1)) * (${FRAME_END} - ${FIRST_FRAME} + 1)")"
+  remain="$(calc "${total} - ${diff}")"
+
+  printf 'Frame computed. (%.2f%%, used: %.1fm, total: %.1fm, remaining: %.1fm, this frame: %ds)\n' "${percent}" "${diff}" "${total}" "${remain}" "${frame_diff}"
+
+  i="$(( i + 1 ))"
+
+  # shellcheck disable=SC2059
+  printf "${RESUME_FMT}" "${diff_sec}" "${FIRST_FRAME}" "$0" "${FUNCTION}" "${FILE_PATTERN}" "${i}" "${FRAME_END}" >resume.sh
   chmod +x resume.sh
 done
 
 rm -f resume.sh
 
+num_frames="$(( FRAME_END - FRAME_START + 1 ))"
+total="$(calc "(${time_end} - ${TIME_START}) / 60")"
+
+printf 'Computed %d frames in %.1fm.\n' "${num_frames}" "${total}"
